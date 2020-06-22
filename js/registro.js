81a02c155695a0e23353d5730f8710f5b9e39217
rvg <alexandrat@codefabric.io>
2020-06-22 19:51:41 -0400

    Enlaces de redes sociales agregados. Validacion de registro mejorada.

--- js/registro.js
+++ js/registro.js
@@ -1,13 +1,29 @@
+function validarEmail(mail) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+    return true;
+  } else {
+    alert("Ingrese una dirección de correo válida.");
+    return false;
+  }
+}
+
 function adaptarEnlaceFormulario(enlace, comodinCorreo, textoEmail) {
   return enlace.replace(comodinCorreo, textoEmail);
 }
 
 function enviarFormulario(elCampoTexto) {
   if (elCampoTexto.value) {
-    urlEnviar = adaptarEnlaceFormulario(enlace, comodinCorreo, elCampoTexto.value)
-    window.open(urlEnviar, '_blank');
+    if (validarEmail(elCampoTexto.value)) {
+      urlEnviar = adaptarEnlaceFormulario(enlace, comodinCorreo, elCampoTexto.value)
+      window.open(urlEnviar, '_blank');
+      return true;
+    } else {
+      return false;
+    }
   } else {
     console.log("Campo email vacio.");
+    alert("Por favor ingrese una dirección de correo electrónico.")
+    return false;
   }
 }
 
@@ -21,8 +37,10 @@ function enviarFormulario2() {
   return enviarFormulario(elCampoTexto);
 }
 
-document.getElementById("formEmail1").onsubmit = enviarFormulario1;
-document.getElementById("formEmail2").onsubmit = enviarFormulario2;
+var formEmail1 = document.getElementById("formEmail1");
+var formEmail2 = document.getElementById("formEmail2");
+formEmail1.onsubmit = enviarFormulario1;
+formEmail2.onsubmit = enviarFormulario2;
 
 var comodinCorreo = "---correo---";
 var enlace = "https://docs.google.com/forms/d/e/1FAIpQLSdqvC-Nww2nQMhT_Kw4rSi7vcu8VDGJ0pdIVeKJ8847tAGWYg/formResponse?usp=pp_url&entry.1506346546=" + comodinCorreo + "&submit=Submit";
